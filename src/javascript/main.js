cf2a5e1c1f6eee080e0c15590b308871a0fee0c5
ckckckcz <rriovld@gmail.com>
2024-07-11 21:29:53 +0700

    feat: best sellers section

--- src/javascript/main.js
+++ src/javascript/main.js
@@ -174,3 +174,35 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 var copy = document.querySelector(".logos-slide").cloneNode(true);
 document.querySelector(".logos").appendChild(copy);
+
+document.addEventListener("DOMContentLoaded", function () {
+  const scrollContainer = document.getElementById("scrollable-buttons");
+  let isDown = false;
+  let startX;
+  let scrollLeft;
+
+  scrollContainer.addEventListener("mousedown", (e) => {
+    isDown = true;
+    scrollContainer.classList.add("active");
+    startX = e.pageX - scrollContainer.offsetLeft;
+    scrollLeft = scrollContainer.scrollLeft;
+  });
+
+  scrollContainer.addEventListener("mouseleave", () => {
+    isDown = false;
+    scrollContainer.classList.remove("active");
+  });
+
+  scrollContainer.addEventListener("mouseup", () => {
+    isDown = false;
+    scrollContainer.classList.remove("active");
+  });
+
+  scrollContainer.addEventListener("mousemove", (e) => {
+    if (!isDown) return;
+    e.preventDefault();
+    const x = e.pageX - scrollContainer.offsetLeft;
+    const walk = (x - startX) * 2; // Adjust scroll speed
+    scrollContainer.scrollLeft = scrollLeft - walk;
+  });
+});
